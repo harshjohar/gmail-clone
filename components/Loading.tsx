b97a52b959d5c149d9f455df4636455cc4299e6f
Harsh Johar <hpsj3112@gmail.com>
2022-05-15 03:28:22 +0530

    lil tricks

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
-
 function Loading() {
   return (
-    <div>Loading</div>
+    <div className='h-screen w-screen overflow-hidden grid place-items-center'>
+      <img src={'./loading.gif'} alt="Gmail" />
+    </div>
   )
 }
 
--- components/Login.tsx
+++ components/Login.tsx
@@ -1,6 +1,7 @@
 import { Button } from '@mui/material'
 import { signInWithPopup } from 'firebase/auth'
 import Head from 'next/head'
+import Image from 'next/image'
 import React from 'react'
 import { auth, provider } from '../Firebase/firebase'
 
@@ -9,7 +10,7 @@ function Login() {
     signInWithPopup(auth, provider)
   }
   return (
-    <div className="grid h-screen w-screen place-items-center overflow-hidden bg-red-400">
+    <div className="grid h-screen w-screen place-items-center overflow-hidden">
       <Head>
         <title>Login</title>
         <link
@@ -17,9 +18,10 @@ function Login() {
           href="https://ssl.gstatic.com/ui/v1/icons/mail/rfr/gmail.ico"
         />
       </Head>
-      <div className="">
+      <div className="flex flex-col items-center space-y-6">
+        <Image src={'https://ssl.gstatic.com/ui/v1/icons/mail/rfr/logo_gmail_lockup_default_2x_r2.png'} height={120} width={300} />
         <Button variant="contained" onClick={() => signIn()}>
-          Login
+          Login With Google
         </Button>
       </div>
     </div>
