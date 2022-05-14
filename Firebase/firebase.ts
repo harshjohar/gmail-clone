37890090127ff315efdf2ac4fa37556c71fdf6c4
Harsh Johar <hpsj3112@gmail.com>
2022-05-14 23:50:22 +0530

    firebase linked

---
+++ Firebase/firebase.ts
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+import { getAuth } from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDK6axWm118FmK-2OnPuTRcTBKLMUvnHZE',
+  authDomain: 'harsh-bf6c2.firebaseapp.com',
+  projectId: 'harsh-bf6c2',
+  storageBucket: 'harsh-bf6c2.appspot.com',
+  messagingSenderId: '992176249879',
+  appId: '1:992176249879:web:48e1a2bffcb6aab7ad5052',
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+const db = getFirestore()
+const auth = getAuth()
+
+export { db, auth }
